77a8722790f2b51e712854773986934d6fd3eba2
Filipe Silvestre & Rick Hills <student@epicodus.com>
2015-08-06 12:32:11 -0700

    added twig functionality and edited html stuff

--- app/app.php
+++ app/app.php
@@ -1,63 +1,64 @@
 <?php
+    // dependencies
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/car.php";
 
-    $app = new Silex\Application();
+    session_start();
+    if (empty($_SESSION['car_list'])) {
+        $_SESSION['car_list'] = array();
+    }
 
-    $app->get("/car_form", function() {
-      return "<!DOCTYPE html>
-              <html>
-              <head>
-                  <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
-                  <title>Find a Car</title>
-              </head>
-              <body>
-                  <div class='container'>
-                      <h1>Find a Car!</h1>
-                      <form action='/car'>
-                          <div class='form-group'>
-                              <label for='price'>Enter Maximum Price:</label>
-                              <input id='price' name='price' class='form-control' type='number'>
-                              <label for='milage'>Enter Maximum Mileage:</label>
-                              <input id='milage' name='milage' class='form-control' type='number'>
-                          </div>
-                          <button type='submit' class='btn-success'>Submit</button>
-                      </form>
-                  </div>
-              </body>
-              </html>";
+    //create and check cookie
+    $app = new Silex\Application();
+    $app['app'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    // ROUTES
+
+        //root page
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('find_cars.html.twig', array('car_list' => Car::getAll()));
     });
 
-    $app->get("/car", function() {
+        //Search for cars passing in parameters
+    $app->get("/display_results", function() use ($app) {
 
-        $porsche = new Car ("2014 Porsche 911", 114991.444, 7864,"images/porsche.jpg");
-        $ford = new Car ("2011 Ford F450", 55995, 14241, "images/ford.jpg");
-        $lexus = new Car ("2013 Lexus RX 350", 44700, 2000,"images/lexus.jpg");
-        $mercedes = new Car ("Mercedes Benz CLS550", 39900, 2222, "images/mercedes.jpg");
+        $porsche = new Car ("2014 Porsche 911", 114991.444, 7864,"porsche.jpg");
+        $ford = new Car ("2011 Ford F450", 55995, 14241, "ford.jpg");
+        $lexus = new Car ("2013 Lexus RX 350", 44700, 2000,"lexus.jpg");
+        $mercedes = new Car ("Mercedes Benz CLS550", 39900, 2222, "mercedes.jpg");
 
         $cars = array($porsche, $ford, $lexus, $mercedes);
 
         $max_price = $_GET['price'];
         $max_milage = $_GET['milage'];
 
-        $output = "";
+        $result_list = array();
+
         foreach ($cars as $car) {
             if ( $car->getPrice() <= $max_price && $car->getMilage() <= $max_milage) {
-                $output = $output . "<li>" . $car->getModel() . "</li>
-                 <li> <img src=" . $car->getPhoto() ."> </li>
-                 <ul>
-                     <li> $".$car->getPrice() . "</li>
-                     <li> Miles:" . $car->getMilage() . "</li>
-                 </ul>
-                 <p>  </p>";
+
+                    array_push($result_list, $car);
+
+
+
+                // $output = $output . "<li>" . $car->getModel() . "</li>
+                //  <li> <img src=" . $car->getPhoto() ."> </li>
+                //  <ul>
+                //      <li> $".$car->getPrice() . "</li>
+                //      <li> Miles:" . $car->getMilage() . "</li>
+                //  </ul>
+                //  <p>  </p>";
+
+
             }
         }
 
-        if ( strlen($output) == 0 ) {
-            $output = "No matches found";
-        }
-
-        return $output;
+        return $app['twig']->render('display_results.html.twig', array('car_list' => $result_list));
 
     });
 
--- src/car.php
+++ src/car.php
@@ -13,16 +13,19 @@ class Car
       $this->miles = $milage;
       $this->photo = $pic;
     }
+
     function setModel ($new_model)
     {
       $string_model = (string) $new_model;
       if (strlen($string_model) > 0)
         $this->make_model = $string_model;
     }
+
     function getModel ()
     {
       return $this->make_model;
     }
+
     function setPrice ($new_price)
     {
       $float_price = (float) $new_price;
@@ -31,6 +34,7 @@ class Car
         $this->price = $formatted_price;
       }
     }
+
     function getPrice ()
     {
       return $this->price;
@@ -44,47 +48,37 @@ class Car
         $this->miles = $formatted_milage;
       }
     }
+
     function getMilage ()
     {
       return $this->miles;
     }
+
     function getPhoto ()
     {
       return $this->photo;
     }
 
-}
-
-
-
-
-// $porsche = new Car ("2014 Porsche 911", 114991.444, 7864,"images/porsche.jpg");
-// $ford = new Car ("2011 Ford F450", 55995, 14241, "images/ford.jpg");
-// $lexus = new Car ("2013 Lexus RX 350", 44700, 2000,"images/lexus.jpg");
-// $mercedes = new Car ("Mercedes Benz CLS550", 39900, 2222, "images/mercedes.jpg");
-//$mercedes->setMilage("10.111");
-//$porsche -> setPrice("1.111111");
-//$lexus->setModel(3);
-
-//$max_price = $_GET["price"];
-//$max_milage = $_GET["milage"];
-
-
-//$cars = array($porsche, $ford, $lexus, $mercedes);
-
-/* $displaycars = array();
-$i = 0;
-foreach ($cars as $car ) {
-    if ( ($car_price = $car->getPrice()) <= $max_price && $car->getMilage() <= $max_milage) {
-        $car_model = $car->getModel();
-        $car_price = $car->getPrice();
-        $car_milage = $car->getMilage();
-        $car_photo = $car->getPhoto();
-        array_push($displaycars, $car_model, $car_price, $car_milage, $car_photo);
-        $display = (string) $displaycars[$i++];
-        echo "Cars: $display";
+    function save()
+    {
+        array_push($_SESSION['car_list'], $this);
     }
+
+    static function getAll()
+    {
+        return $_SESSION['car_list'];
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['car_list'] = array();
+    }
+
+    static function deleteSpecificJob($index)
+    {
+        unset($_SESSION['car_list'][$index]);
+    }
+
 }
-*/
 
 ?>
